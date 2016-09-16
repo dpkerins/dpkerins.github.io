ec689af7ab85f48696f5e695e89bcc651db2b5e6
Doug Kerins <douglaskerins@gmail.com>
2016-09-16 08:23:30 -0400

    Styling, Adding ng-show to arrow, and adding scroll-to directive(did not write)

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,6 @@
 app.controller('MainController', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
 	var ref = firebase.database().ref('WelcomePage/');
 	$scope.welcomePage = $firebaseObject(ref);
+
+	var arrowHover = false;
 }]);
